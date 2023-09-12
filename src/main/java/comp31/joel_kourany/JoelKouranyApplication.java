648e88e76047272ae95e0e330acc860fd7cc4ad5
JoelFKG <joelfarid.kouranygarcia@student.sl.on.ca>
2023-09-12 15:29:31 -0400

    Lab1 - Simple Program

---
+++ src/main/java/comp31/joel_kourany/JoelKouranyApplication.java
@@ -0,0 +1,13 @@
+package comp31.joel_kourany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoelKouranyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoelKouranyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comp31/joel_kourany/JoelKouranyApplicationTests.java
@@ -0,0 +1,13 @@
+package comp31.joel_kourany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoelKouranyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
